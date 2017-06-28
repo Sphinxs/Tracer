5a421f176df1780aa0cf733d3c0a58a1a7fcb954
Walter <Sphinxs@users.noreply.github.com>
2017-06-27 22:44:06 -0300

    Class

--- Tracer.py
+++ Tracer.py
@@ -16,6 +16,7 @@ class Db ( object ) :
 
         self.cur = self.con.cursor ()
 
+
     def table ( self, * args ) :
 
         ''' Args = 'Itens', ( 'Field1 text, Field2 integer' ) '''
@@ -26,27 +27,46 @@ class Db ( object ) :
 
             self.con.commit ()
 
-        except sqlite3.Error as Er :
+        except sqlite3.Error as Ert :
 
-            raise ( 'Nenhuma tabela foi criada ou os parâmetros contém erros - {0}'.foramt ( Er ) )
+            raise ( 'Nenhuma tabela foi criada, os parâmetros podem conter erros - {0}'.foramt ( Ert ) )
+
         else :
 
             print ( 'A tabela {0} foi criada'.format ( args[0] ) )
-            
+
+
     def insert ( self, * args ) :
 
         ''' Args = 'Name-Table', ( 'Field1', 'Field2' ), ( 'Value-Field1', 02 ) '''
 
-        self.cur.execute ( '''insert into {0} {1} values {2}'''.format ( args[0], args[1], args[2] ) )
+        try :
 
-        self.con.commit ()
+            self.cur.execute ( '''insert into {0} {1} values {2}'''.format ( args[0], args[1], args[2] ) )
+
+            self.con.commit ()
+
+        except sqlite3.Error as Eri :
+
+            raise ( 'Nenhum item foi adicionado, os parâmetros podem conter erros - {0}'.foramt ( Eri ) )
+
+        else :
+
+            print ( 'Informações adicionadas com sucesso'.format ( args[0] ) )
+
 
     def show ( self, tbl = None ) :
 
         ''' Tbl = 'Example' '''
 
-        self.cur.execute ( '''select * from {0}'''.format ( tbl ) )
+        try :
 
-        for row in self.cur.execute ( '''select * from {0}'''.format ( tbl ) ) :
+            self.cur.execute ( '''select * from {0}'''.format ( tbl ) )
 
-            print ( '{0}'.format ( row ) )
+            for row in self.cur.execute ( '''select * from {0}'''.format ( tbl ) ) :
+
+                print ( '{0}'.format ( row ) )
+
+        except sqlite3.Error as Ers :
+
+            raise ( 'Nenhuma tabela foi encontrada, os parâmetros podem conter erros - {0}'.format ( Ers ) )
